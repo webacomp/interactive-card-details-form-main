8988efbf86f59f542ebd6048b87bfc98548ec628
nika <webacomp@yahoo.com>
2022-11-29 13:02:31 +0100

    add js functionaly 	modified:   files/_mixins.scss 	modified:   files/main.css 	modified:   files/main.css.map 	modified:   files/main.scss 	modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -2,29 +2,46 @@ const nameEl = document.querySelector('.name');
 const numberEl = document.querySelector('.number');
 const datumMonthEl = document.getElementById('datum-mm');
 const datumYearEl = document.getElementById('datum-yy');
+const cvvEl = document.getElementById('cvv');
+
 const btnConfirm = document.getElementById('confirm');
-const cvvEl = document.getElementById('cvv')
+const btnContinue = document.getElementById('continue');
+
+const cardContainer = document.querySelector('.card__container');
 
 const userName = document.querySelector('.header__row-data p');
 const userNumbers = document.querySelector('.header__card-numbers');
-
 const cardDatum = document.querySelector('.header__row-data span');
-const cvv = document.getElementById('card_cvv')
+const cvv = document.getElementById('card_cvv');
 
-const btnContinue = document.getElementById('continue');
 const final = document.querySelector('.final');
-
 const month = document.getElementById('month');
 const year = document.getElementById('year');
-userName.textContent = 'jane Applessed';
 
+const numbers = /^[0-9]+$/;
 
+function initDataCard() {
+	userName.textContent = 'Jane Applessed';
+	userNumbers.textContent = '0000 0000 0000 0000';
+	month.textContent = '00 /';
+	year.textContent = '00';
+}
+initDataCard();
+//Inputs
 
 nameEl.addEventListener('input', (e) => {
 	userName.textContent = e.target.value;
 });
 numberEl.addEventListener('input', (e) => {
 	userNumbers.textContent = e.target.value;
+
+	if (!e.target.value.match(numbers)) {
+		numberEl.classList.add('wrong-value');
+	}
+	
+	if(e.target.value.match(numbers)) {
+		numberEl.classList.remove('wrong-value');
+	}
 });
 
 datumMonthEl.addEventListener('input', (e) => {
@@ -34,24 +51,39 @@ datumYearEl.addEventListener('input', (e) => {
 	year.textContent = e.target.value;
 });
 
-cvvEl.addEventListener('input', e => {
-    cvv.innerHTML = e.target.value
-})
+cvvEl.addEventListener('input', (e) => {
+	cvv.textContent = e.target.value;
+});
 
+//Button Events
 btnConfirm.addEventListener('click', (_) => {
 	if (!final.classList.contains('show')) final.classList.add('show');
+
+	cardContainer.style.display = 'none';
 });
 btnContinue.addEventListener('click', (_) => {
 	if (final.classList.contains('show')) final.classList.remove('show');
 
-	deleteDataFromInput()
+	cardContainer.style.display = 'block';
+
+	deleteDataFromInput();
+
+	putOutData();
 });
 
-
+//Delete text from input
 const deleteDataFromInput = function () {
-	nameEl.value = ''
-	datumMonthEl.value = ''
-	datumYearEl.value = ''
-	numberEl.value = ''
-	cvvEl.value = ''
-}
+	nameEl.value = '';
+	datumMonthEl.value = '';
+	datumYearEl.value = '';
+	numberEl.value = '';
+	cvvEl.value = '';
+};
+
+//Display init data
+const putOutData = function () {
+	userName.textContent = 'Jane Applessed';
+	userNumbers.textContent = '0000 0000 0000 0000';
+	month.textContent = '00 /';
+	year.textContent = '00';
+};
